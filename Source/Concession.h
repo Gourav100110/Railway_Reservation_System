01d4b6370c08bfa92f51d488a52c3d072390902f
Gourav Preet Gupta <147718991+Gourav100110@users.noreply.github.com>
2025-11-12 18:54:05 +0530

    Add files via upload

---
+++ Source/Concession.h
@@ -0,0 +1,13 @@
+#ifndef CONCESSION_H
+#define CONCESSION_H
+#include <iostream>
+#include "BookingCategory.h"
+using namespace std;
+
+class Concession : public BookingCategory
+{
+protected:
+    Concession(string name) : BookingCategory(name) {}
+};
+
+#endif
---
+++ Source/Divyang.h
@@ -0,0 +1,13 @@
+#ifndef Divyang_H
+#define Divyang_H
+#include <iostream>
+#include "Concession.h"
+using namespace std;
+
+class Divyang : public Concession
+{
+protected:
+    Divyang(string name) : Concession(name) {}
+};
+
+#endif
